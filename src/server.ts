b5d099f7b4f53233f3f50ea6efdc143d5a400f05
Jallox <javiluky2009@gmail.com>
2025-10-20 21:46:13 +0200

    refactor(main): remove extensions from imports

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
-import app from "./app.js";
-import { connectDB } from "./config/db.config.js";
+import app from "./app";
+import { connectDB } from "./config/db.config";
 
 dotenv.config();
 
